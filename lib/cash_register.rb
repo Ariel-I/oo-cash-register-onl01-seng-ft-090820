499051bc9b93bd83f7751fe2b5a59598894bfc62
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-20 05:19:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,19 +9,7 @@ def initialize(discount=0)
 end 
 
 def add_item(title, price, number_of_items = 1)
-  if number_of_items > 1 
-    i = 0 
-    while i < number_of_items
-    @items << title 
-    i += 1 
-  end 
-  else 
-    @items << title 
-end 
-  @total += price*number_of_items
-  @last_transaction = @total
-  @total
-end 
+ 
 
 def apply_discount()
   if @discount > 0  
