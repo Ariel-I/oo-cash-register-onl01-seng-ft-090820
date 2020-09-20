9733991cba7416cf7d5cc4252eb34ab99fde933c
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-20 04:42:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,2 +1,22 @@
 class CashRegister
+
+attr_accessor :name, :total, :discount 
+
+def initialize(total)
+  @total = total
+  @discount = discount
+end 
+
+def total 
+end 
+
+def add_item
+end 
+
+def apply_discount
+end 
+
+
+
+
 end 
